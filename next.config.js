362a2b695c30572eb906768f416a74b9c00a7232
aldhanekaa <aldhanekadev@gmail.com>
2021-11-01 12:42:46 +0700

    change nextjs.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
-const withTM = require("@vercel/edge-functions-ui/transpile")();
-
-module.exports = withTM();
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+};
