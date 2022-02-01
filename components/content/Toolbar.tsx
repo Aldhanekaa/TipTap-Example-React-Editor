7cff6c89d555f3b6380bade20041b05825fb25dd
aldhanekaa <aldhanekadev@gmail.com>
2022-02-01 16:27:16 +0700

    make toolbar sticky

--- components/content/Toolbar.tsx
+++ components/content/Toolbar.tsx
@@ -50,293 +50,290 @@ const ProjectCreateContentToolbar = ({ editor }: { editor: Editor }) => {
 
   return (
     <>
-      <div>
-        <Paper
-          elevation={0}
-          sx={{
-            display: "flex",
-            border: (theme) => `1px solid ${theme.palette.divider}`,
-            flexWrap: "wrap",
-            mb: 2,
-          }}
+      <Paper
+        elevation={0}
+        sx={{
+          display: "flex",
+          border: (theme) => `1px solid ${theme.palette.divider}`,
+          flexWrap: "wrap",
+          mb: 2,
+          position: "sticky",
+          top: 10,
+          zIndex: 9999,
+        }}
+      >
+        <HeadingToolbarButtons editor={editor} />
+        <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
+        <StyledToggleButtonGroup
+          size="small"
+          exclusive
+          aria-label="text alignment"
         >
-          <HeadingToolbarButtons editor={editor} />
-          <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
-          <StyledToggleButtonGroup
-            size="small"
-            exclusive
-            aria-label="text alignment"
+          <ToggleButton
+            onClick={() => editor.chain().focus().setTextAlign("left").run()}
+            selected={editor.isActive({ textAlign: "left" })}
+            value="left"
+            aria-label="left aligned"
           >
-            <ToggleButton
-              onClick={() => editor.chain().focus().setTextAlign("left").run()}
-              selected={editor.isActive({ textAlign: "left" })}
-              value="left"
-              aria-label="left aligned"
-            >
-              <FormatAlignLeftIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() =>
-                editor.chain().focus().setTextAlign("center").run()
+            <FormatAlignLeftIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().setTextAlign("center").run()}
+            selected={editor.isActive({ textAlign: "center" })}
+            value="center"
+            aria-label="Center aligned"
+          >
+            <FormatAlignCenterIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().setTextAlign("right").run()}
+            selected={editor.isActive({ textAlign: "right" })}
+            value="right"
+            aria-label="Right aligned"
+          >
+            <FormatAlignRightIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().setTextAlign("justify").run()}
+            selected={editor.isActive({ textAlign: "justify" })}
+            value="justify"
+            aria-label="Justify aligned"
+          >
+            <FormatAlignJustifyIcon />
+          </ToggleButton>
+        </StyledToggleButtonGroup>
+        <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
+
+        <StyledToggleButtonGroup size="small" aria-label="text formatting">
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleSuperscript().run()}
+            selected={editor.isActive("superscript")}
+            value="superscript"
+            aria-label="superscript"
+          >
+            <SuperscriptIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleSubscript().run()}
+            selected={editor.isActive("subscript")}
+            value="subscript"
+            aria-label="subscript"
+          >
+            <SubscriptIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            selected={editor.isActive("bold")}
+            value="bold"
+            aria-label="bold"
+          >
+            <FormatBoldIcon />
+          </ToggleButton>
+
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            value="italic"
+            aria-label="italic"
+            selected={editor.isActive("italic")}
+          >
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            value="strike"
+            aria-label="strike"
+            selected={editor.isActive("strike")}
+          >
+            <FormatStrikethroughIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleCode().run()}
+            value="code"
+            aria-label="code"
+            selected={editor.isActive("code")}
+          >
+            <CodeIcon />
+          </ToggleButton>
+
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleHighlight().run()}
+            value="highlight"
+            aria-label="highlight"
+            selected={editor.isActive("highlight")}
+          >
+            <BorderColorIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleBlockquote().run()}
+            value="blockQuote"
+            aria-label="blockQuote"
+            selected={editor.isActive("blockQuote")}
+          >
+            <FormatQuoteIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().setHorizontalRule().run()}
+            selected={editor.isActive("HorizontalRule")}
+            value="HorizontalRule"
+            aria-label="HorizontalRule"
+          >
+            <HorizontalRuleIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().setParagraph().run()}
+            selected={editor.isActive("paragraph")}
+            value="paragraph"
+            aria-label="paragraph"
+          >
+            <FormatTextdirectionRToLIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleUnderline().run()}
+            selected={editor.isActive("underline")}
+            value="underline"
+            aria-label="underline"
+          >
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => {
+              setOpenPickImage(true);
+            }}
+            selected={editor.isActive("image-renderer")}
+            value="image-renderer"
+            aria-label="image-renderer"
+          >
+            <ImageIcon />
+          </ToggleButton>
+          <PickImage
+            open={OpenPickImage}
+            handleClose={() => setOpenPickImage(false)}
+            setThumbnail={(value: { src: string; alt?: string }) => {
+              editor
+                .chain()
+                .focus()
+                // @ts-ignore
+                .setImage({ src: value.src, alt: value.alt })
+                .run();
+            }}
+          />
+          <ToggleButton
+            onClick={() => {
+              console.log(editor.state);
+              setOpenPickVideo(true);
+            }}
+            selected={editor.isActive("videoPlayer")}
+            value="videoPlayer"
+            aria-label="videoPlayer"
+          >
+            <VideoLibraryIcon />
+          </ToggleButton>
+          <PickVideo
+            open={OpenPickVideo}
+            handleClose={() => setOpenPickVideo(false)}
+            setThumbnail={(value: { src: string }) => {
+              editor.chain().focus().setVideo({ src: value.src }).run();
+            }}
+          />
+          <ToggleButton
+            onClick={() => {
+              const previousUrl = editor.getAttributes("link").href;
+              const url = window.prompt("URL", previousUrl);
+
+              // cancelled
+              if (url === null) {
+                return;
               }
-              selected={editor.isActive({ textAlign: "center" })}
-              value="center"
-              aria-label="Center aligned"
-            >
-              <FormatAlignCenterIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().setTextAlign("right").run()}
-              selected={editor.isActive({ textAlign: "right" })}
-              value="right"
-              aria-label="Right aligned"
-            >
-              <FormatAlignRightIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() =>
-                editor.chain().focus().setTextAlign("justify").run()
-              }
-              selected={editor.isActive({ textAlign: "justify" })}
-              value="justify"
-              aria-label="Justify aligned"
-            >
-              <FormatAlignJustifyIcon />
-            </ToggleButton>
-          </StyledToggleButtonGroup>
-          <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
 
-          <StyledToggleButtonGroup size="small" aria-label="text formatting">
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleSuperscript().run()}
-              selected={editor.isActive("superscript")}
-              value="superscript"
-              aria-label="superscript"
-            >
-              <SuperscriptIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleSubscript().run()}
-              selected={editor.isActive("subscript")}
-              value="subscript"
-              aria-label="subscript"
-            >
-              <SubscriptIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleBold().run()}
-              selected={editor.isActive("bold")}
-              value="bold"
-              aria-label="bold"
-            >
-              <FormatBoldIcon />
-            </ToggleButton>
-
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleItalic().run()}
-              value="italic"
-              aria-label="italic"
-              selected={editor.isActive("italic")}
-            >
-              <FormatItalicIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleStrike().run()}
-              value="strike"
-              aria-label="strike"
-              selected={editor.isActive("strike")}
-            >
-              <FormatStrikethroughIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleCode().run()}
-              value="code"
-              aria-label="code"
-              selected={editor.isActive("code")}
-            >
-              <CodeIcon />
-            </ToggleButton>
-
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleHighlight().run()}
-              value="highlight"
-              aria-label="highlight"
-              selected={editor.isActive("highlight")}
-            >
-              <BorderColorIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleBlockquote().run()}
-              value="blockQuote"
-              aria-label="blockQuote"
-              selected={editor.isActive("blockQuote")}
-            >
-              <FormatQuoteIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().setHorizontalRule().run()}
-              selected={editor.isActive("HorizontalRule")}
-              value="HorizontalRule"
-              aria-label="HorizontalRule"
-            >
-              <HorizontalRuleIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().setParagraph().run()}
-              selected={editor.isActive("paragraph")}
-              value="paragraph"
-              aria-label="paragraph"
-            >
-              <FormatTextdirectionRToLIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleUnderline().run()}
-              selected={editor.isActive("underline")}
-              value="underline"
-              aria-label="underline"
-            >
-              <FormatUnderlinedIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => {
-                setOpenPickImage(true);
-              }}
-              selected={editor.isActive("image-renderer")}
-              value="image-renderer"
-              aria-label="image-renderer"
-            >
-              <ImageIcon />
-            </ToggleButton>
-            <PickImage
-              open={OpenPickImage}
-              handleClose={() => setOpenPickImage(false)}
-              setThumbnail={(value: { src: string; alt?: string }) => {
-                editor
-                  .chain()
-                  .focus()
-                  // @ts-ignore
-                  .setImage({ src: value.src, alt: value.alt })
-                  .run();
-              }}
-            />
-            <ToggleButton
-              onClick={() => {
-                console.log(editor.state);
-                setOpenPickVideo(true);
-              }}
-              selected={editor.isActive("videoPlayer")}
-              value="videoPlayer"
-              aria-label="videoPlayer"
-            >
-              <VideoLibraryIcon />
-            </ToggleButton>
-            <PickVideo
-              open={OpenPickVideo}
-              handleClose={() => setOpenPickVideo(false)}
-              setThumbnail={(value: { src: string }) => {
-                editor.chain().focus().setVideo({ src: value.src }).run();
-              }}
-            />
-            <ToggleButton
-              onClick={() => {
-                const previousUrl = editor.getAttributes("link").href;
-                const url = window.prompt("URL", previousUrl);
-
-                // cancelled
-                if (url === null) {
-                  return;
-                }
-
-                // empty
-                if (url === "") {
-                  editor
-                    .chain()
-                    .focus()
-                    .extendMarkRange("link")
-                    .unsetLink()
-                    .run();
-
-                  return;
-                }
-
-                // update link
+              // empty
+              if (url === "") {
                 editor
                   .chain()
                   .focus()
                   .extendMarkRange("link")
-                  .setLink({ href: url })
+                  .unsetLink()
                   .run();
-              }}
-              selected={editor.isActive("link")}
-              value="link"
-              aria-label="link"
-            >
-              <LinkIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleBulletList().run()}
-              value="bullettList"
-              aria-label="bullettList"
-              selected={editor.isActive("bulletList")}
-            >
-              <FormatListBulletedIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().toggleOrderedList().run()}
-              value="orderedList"
-              aria-label="orderedList"
-              selected={editor.isActive("orderedList")}
-            >
-              <FormatListNumberedIcon />
-            </ToggleButton>
-          </StyledToggleButtonGroup>
-          <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
 
-          <StyledToggleButtonGroup
-            size="small"
-            exclusive
-            aria-label="text alignment"
+                return;
+              }
+
+              // update link
+              editor
+                .chain()
+                .focus()
+                .extendMarkRange("link")
+                .setLink({ href: url })
+                .run();
+            }}
+            selected={editor.isActive("link")}
+            value="link"
+            aria-label="link"
           >
-            <ToggleButton
-              onClick={() => editor.chain().focus().undo().run()}
-              value="undo"
-              aria-label="undo"
-            >
-              <UndoIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().redo().run()}
-              value="redo"
-              aria-label="redo"
-            >
-              <RedoIcon />
-            </ToggleButton>
-          </StyledToggleButtonGroup>
-
-          <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
-
-          <StyledToggleButtonGroup
-            size="small"
-            exclusive
-            aria-label="text alignment"
+            <LinkIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            value="bullettList"
+            aria-label="bullettList"
+            selected={editor.isActive("bulletList")}
           >
-            <ToggleButton
-              onClick={() => editor.chain().focus().unsetAllMarks().run()}
-              value="clear-mark"
-              aria-label="clear-mark"
-            >
-              <LayersClearIcon />
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => editor.chain().focus().clearNodes().run()}
-              value="clear-node"
-              aria-label="clear-node"
-            >
-              <ClearIcon />
-            </ToggleButton>
-          </StyledToggleButtonGroup>
-        </Paper>
-      </div>
+            <FormatListBulletedIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().toggleOrderedList().run()}
+            value="orderedList"
+            aria-label="orderedList"
+            selected={editor.isActive("orderedList")}
+          >
+            <FormatListNumberedIcon />
+          </ToggleButton>
+        </StyledToggleButtonGroup>
+        <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
+
+        <StyledToggleButtonGroup
+          size="small"
+          exclusive
+          aria-label="text alignment"
+        >
+          <ToggleButton
+            onClick={() => editor.chain().focus().undo().run()}
+            value="undo"
+            aria-label="undo"
+          >
+            <UndoIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().redo().run()}
+            value="redo"
+            aria-label="redo"
+          >
+            <RedoIcon />
+          </ToggleButton>
+        </StyledToggleButtonGroup>
+
+        <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
+
+        <StyledToggleButtonGroup
+          size="small"
+          exclusive
+          aria-label="text alignment"
+        >
+          <ToggleButton
+            onClick={() => editor.chain().focus().unsetAllMarks().run()}
+            value="clear-mark"
+            aria-label="clear-mark"
+          >
+            <LayersClearIcon />
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => editor.chain().focus().clearNodes().run()}
+            value="clear-node"
+            aria-label="clear-node"
+          >
+            <ClearIcon />
+          </ToggleButton>
+        </StyledToggleButtonGroup>
+      </Paper>
     </>
   );
 };
